2111b11e4530cfd09cc46b1b3d43b336449e9e97
m3trik@live.com <m3trik@live.com>
2023-10-26 20:17:36 -0400

    1.0.0
    - signals: Added block signals decorator.
    - widgets.comboBox: ComboBox now hides the header from the drop down menu and automatically resets to index -1 (the header) after an item selection.  When a header is not given, the combo box acts as normal.  Also, the header alignment now works.
    - widgets.mixins.style_sheet: Updated the combo box style sheet to give more control over padding.
    -

--- uitk/widgets/comboBox.py
+++ uitk/widgets/comboBox.py
@@ -1,91 +1,109 @@
 # !/usr/bin/python
 # coding=utf-8
+import re
 from PySide2 import QtCore, QtGui, QtWidgets
+from uitk.signals import Signals
 from uitk.widgets.menu import Menu
 from uitk.widgets.mixins.attributes import AttributesMixin
 from uitk.widgets.mixins.text import RichText, TextOverlay
 
 
-class CustomListView(QtWidgets.QListView):
-    def __init__(self, parent):
-        super().__init__(parent)
-        self.combobox = parent  # Store a reference to the parent combobox
+class CustomStyle(QtWidgets.QProxyStyle):
+    def __init__(self, style):
+        super().__init__(style)
+        self.combo_box = None  # Initialize to None, will be set later
 
-    def paintEvent(self, event):
-        if self.combobox.header_text:  # Check if a header is present
-            # Get the index of the first item
-            index = self.model().index(0, 0)
-            # Get the item rectangle
-            rect = self.visualRect(index)
-            # Check if the event rect intersects with the first item rectangle
-            if event.rect().intersects(rect):
-                # Adjust the event rect to exclude the first item rectangle
-                event = QtGui.QPaintEvent(event.rect().translated(0, rect.height()))
-        super().paintEvent(event)
+    def drawControl(self, element, opt, painter, widget=None):
+        if widget is None or isinstance(widget, AlignedComboBox):
+            if element == QtWidgets.QStyle.CE_ComboBoxLabel:
+                current_index = self.combo_box.currentIndex()
+                if self.combo_box.has_header:
+                    current_index -= 1
+
+                if self.combo_box.header_text and (current_index == -1):
+                    opt.text = self.combo_box.header_text
+                    opt.displayAlignment = self.combo_box.header_alignment
+                elif current_index >= 0:
+                    opt.text = self.combo_box.itemText(current_index)
+
+        super().drawControl(element, opt, painter, widget)
 
 
 class AlignedComboBox(QtWidgets.QComboBox):
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.header_alignment = QtCore.Qt.AlignLeft
-        self.header_text = None  # New field to store the header text
-        self.setView(CustomListView(self))  # Set the custom view
+        self.header_text = None
+        self.header_alignment = QtCore.Qt.AlignCenter
 
-    def setHeader(self, header, alignment="left"):
-        self.header_text = header
-        self.setHeaderAlignment(alignment)
-        self.insertItem(0, header)
-        self.setCurrentIndex(0)
+        self.custom_style = CustomStyle(self.style())
+        self.custom_style.combo_box = self  # Set the combo_box reference here
+        self.setStyle(self.custom_style)
+
+    def setHeaderText(self, text):
+        self.header_text = text
+        self.update()
 
     def setHeaderAlignment(self, alignment):
-        if isinstance(alignment, str):
-            alignment_map = {
-                "left": QtCore.Qt.AlignLeft,
-                "center": QtCore.Qt.AlignCenter,
-                "right": QtCore.Qt.AlignRight,
-            }
-            self.header_alignment = alignment_map.get(
-                alignment.lower(), QtCore.Qt.AlignLeft
-            )
-        elif isinstance(alignment, QtCore.Qt.AlignmentFlag):
-            self.header_alignment = alignment
+        if alignment == "left":
+            self.header_alignment = QtCore.Qt.AlignLeft
+        elif alignment == "right":
+            self.header_alignment = QtCore.Qt.AlignRight
+        elif alignment == "center":
+            self.header_alignment = QtCore.Qt.AlignHCenter
         else:
-            raise ValueError(f"Unsupported alignment type: {type(alignment)}")
+            self.header_alignment = QtCore.Qt.AlignLeft
+        self.update()
+
+    def get_stylesheet_property(self, property_name):
+        stylesheet = self.styleSheet()
+        match = re.search(f"{property_name} *: *(\d+)", stylesheet)
+        if match:
+            return int(match.group(1))
+        return 0
 
     def paintEvent(self, event):
-        painter = QtGui.QPainter(self)
-        painter.fillRect(event.rect(), self.palette().base())
-
-        if self.currentIndex() == 0 and self.header_text:
-            painter.save()
-            rect = event.rect().adjusted(4, 0, 0, 0)  # Adjust margin if needed
-            painter.drawText(rect, self.header_alignment, self.currentText())
-            painter.restore()
-
-        painter.end()
+        # Always call the parent class's paintEvent to ensure all elements are drawn
         super().paintEvent(event)
 
-    def showPopup(self):
-        self.model().showingPopup = True  # Set flag before showing popup
-        super().showPopup()
+        # Then do custom paint for the header text
+        if self.header_text and self.currentIndex() == -1:
+            painter = QtGui.QPainter(self)
 
-    def hidePopup(self):
-        self.model().showingPopup = False  # Reset flag after hiding popup
-        super().hidePopup()
+            # Get the text color from the stylesheet
+            color = self.palette().color(QtGui.QPalette.Text)
+            painter.setPen(color)
+
+            # Retrieve padding from the stylesheet
+            left_padding = self.get_stylesheet_property("padding-left")
+            right_padding = self.get_stylesheet_property("padding-right")
+            top_padding = self.get_stylesheet_property("padding-top")
+            bottom_padding = self.get_stylesheet_property("padding-bottom")
+
+            rect = self.rect().adjusted(
+                left_padding, top_padding, -right_padding, -bottom_padding
+            )
+
+            alignment = self.header_alignment
+            painter.drawText(rect, alignment | QtCore.Qt.AlignVCenter, self.header_text)
+            painter.end()
 
 
 class ComboBox(AlignedComboBox, AttributesMixin, RichText, TextOverlay):
-    """Custom ComboBox widget with additional features and custom signal handling."""
-
     before_popup_shown = QtCore.Signal()
     on_editing_finished = QtCore.Signal(str)
     on_item_deleted = QtCore.Signal(str)
 
     def __init__(self, parent=None, editable=False, **kwargs):
         super().__init__(parent)
+        self.restore_previous_index = False
+        self.prev_index = -1
+        self.has_header = False
+        self.header_text = None
 
         self.editable = editable
         self.menu = Menu(self, mode="option", fixed_item_height=20)
+
+        self.currentIndexChanged.connect(self.check_index)
 
         self.set_attributes(**kwargs)
 
@@ -96,15 +114,99 @@ class ComboBox(AlignedComboBox, AttributesMixin, RichText, TextOverlay):
             for i in range(self.count())
         ]
 
-    def block_signals(fn):
-        def wrapper(self, *args, **kwargs):
+    @Signals.blockSignals
+    def currentData(self):
+        return self.itemData(self.currentIndex())
+
+    @Signals.blockSignals
+    def setCurrentData(self, value):
+        self.setItemData(self.currentIndex(), value)
+
+    @Signals.blockSignals
+    def currentText(self):
+        return self.richText(self.currentIndex())
+
+    @Signals.blockSignals
+    def setCurrentText(self, text):
+        self.setRichText(text, self.currentIndex())
+
+    @Signals.blockSignals
+    def setItemText(self, index, text):
+        self.setRichText(text, index)
+
+    def setAsCurrent(self, i, blockSignals=False):
+        if blockSignals:
             self.blockSignals(True)
-            rtn = fn(self, *args, **kwargs)
+
+        try:
+            index = (
+                self.items.index(i)
+                if isinstance(i, str)
+                else i
+                if isinstance(i, int)
+                else None
+            )
+        except ValueError:
+            raise ValueError(
+                f"The item '{i}' was not found in ComboBox. Available items are {self.items}."
+            )
+
+        if index is None:
+            raise RuntimeError(
+                f"Failed to set current item in ComboBox: expected int or str, got {i, type(i)}"
+            )
+
+        self.setCurrentIndex(index)
+
+        if blockSignals:
             self.blockSignals(False)
-            return rtn
 
-        return wrapper
+    def setCurrentIndex(self, index):
+        if index < 0:
+            self.blockSignals(True)
+        super().setCurrentIndex(index)
+        if index < 0:
+            self.blockSignals(False)
 
+    def check_index(self, index):
+        if self.has_header and index != -1:
+            clicked_item_text = self.itemText(index)
+            self.setCurrentText(clicked_item_text)
+            self.setCurrentIndex(-1)
+        elif index == -1 and self.has_header:
+            self.setEditText(self.header_text)
+
+    def setEditable(self, editable):
+        if editable:
+            current_text = self.currentText()
+            super().setEditable(True)
+            lineEdit = self.lineEdit()
+            lineEdit.setText(current_text)
+            lineEdit.deselect()
+        else:
+            lineEdit = self.lineEdit()
+            new_text = lineEdit.text()
+            super().setEditable(False)
+            self.setCurrentText(new_text)
+            self.on_editing_finished.emit(new_text)
+
+    def force_header_display(self):
+        if self.header_text:
+            super().setCurrentIndex(-1)
+
+    def add_header(self, text):
+        self.insertItem(0, text)
+        self.header_text = text
+        self.has_header = True
+        self.setCurrentIndex(-1)
+
+    def add_single(self, item, data, ascending):
+        if ascending:
+            self.insertItem(0, item, data)
+        else:
+            self.addItem(item, data)
+
+    @Signals.blockSignals
     def add(
         self,
         x,
@@ -135,32 +237,28 @@ class ComboBox(AlignedComboBox, AttributesMixin, RichText, TextOverlay):
         Raises:
             TypeError: If the type of 'x' is unsupported.
         """
-        self.blockSignals(True)
-
-        last_added_data = None
-        last_added_text = None
-
-        def add_single(item, data):
-            nonlocal last_added_data, last_added_text
-            if ascending:
-                self.insertItem(0, item, data)
-            else:
-                self.addItem(item, data)
-            last_added_data = data
-            last_added_text = item
+        self.restore_previous_index = restore_index
+        if restore_index:
+            self.prev_index = self.currentIndex()
 
         if not _recursion and clear:
-            prev_index = self.currentIndex()
             self.clear()
 
+        if header:
+            self.setHeaderText(header)
+            self.setHeaderAlignment(header_alignment)
+            self.has_header = True  # Set has_header to True
+        else:
+            self.has_header = False  # Set has_header to False
+
         if isinstance(x, dict):
-            [add_single(k, v) for k, v in x.items()]
+            [self.add_single(k, v, ascending) for k, v in x.items()]
         elif isinstance(x, (list, tuple, set)):
-            [add_single(item, data) for item in x]
+            [self.add_single(item, data, ascending) for item in x]
         elif isinstance(x, (zip, map)):
-            [add_single(i, d) for i, d in x]
+            [self.add_single(i, d, ascending) for i, d in x]
         elif isinstance(x, str):
-            add_single(x, data)
+            self.add_single(x, data, ascending)
         else:
             raise TypeError(
                 f"Unsupported item type: '{type(x)}'. Expected str, list, tuple, set, map, zip, or dict."
@@ -168,22 +266,23 @@ class ComboBox(AlignedComboBox, AttributesMixin, RichText, TextOverlay):
 
         self.set_attributes(**kwargs)
 
+        # At the end of the add method
         if not _recursion:
-            self.blockSignals(True)
+            if header or self.header_text:
+                self.force_header_display()
+            # Default index based on header presence
+            final_index = -1 if self.header_text else 0
 
-            final_index = 0  # Default index is 0
-            if restore_index and prev_index > 0:
-                final_index = prev_index
+            if restore_index and self.prev_index > -1:
+                self.setCurrentIndex(self.prev_index)
+                final_index = self.prev_index
+            elif self.header_text:
+                self.prev_index = -1  # Set prev_index to header index
+                final_index = -1  # Force the header display
 
-            self.setCurrentIndex(final_index)
-
-            if header:
-                self.setHeader(header, header_alignment)
-
-            self.blockSignals(False)
             self.currentIndexChanged.emit(final_index)
 
-    @block_signals
+    @Signals.blockSignals
     def removeItem(self, index=None):
         if index is None:
             index = self.currentIndex()
@@ -191,69 +290,8 @@ class ComboBox(AlignedComboBox, AttributesMixin, RichText, TextOverlay):
         super().removeItem(index)
         self.item_deleted.emit(item_text)
 
-    @block_signals
-    def currentData(self):
-        return self.itemData(self.currentIndex())
-
-    @block_signals
-    def setCurrentData(self, value):
-        self.setItemData(self.currentIndex(), value)
-
-    @block_signals
-    def currentText(self):
-        return self.richText(self.currentIndex())
-
-    @block_signals
-    def setCurrentText(self, text):
-        self.setRichText(text, self.currentIndex())
-
-    @block_signals
-    def setItemText(self, index, text):
-        self.setRichText(text, index)
-
-    def setCurrentItem(self, i, block_signals=False):
-        if block_signals:
-            self.blockSignals(True)
-
-        try:
-            index = (
-                self.items.index(i)
-                if isinstance(i, str)
-                else i
-                if isinstance(i, int)
-                else None
-            )
-        except ValueError:
-            raise ValueError(
-                f"The item '{i}' was not found in ComboBox. Available items are {self.items}."
-            )
-
-        if index is None:
-            raise RuntimeError(
-                f"Failed to set current item in ComboBox: expected int or str, got {i, type(i)}"
-            )
-
-        self.setCurrentIndex(index)
-
-        if block_signals:
-            self.blockSignals(False)
-
-    def setEditable(self, editable):
-        if editable:
-            current_text = self.currentText()
-            super().setEditable(True)
-            lineEdit = self.lineEdit()
-            lineEdit.setText(current_text)
-            lineEdit.deselect()
-        else:
-            lineEdit = self.lineEdit()
-            new_text = lineEdit.text()
-            super().setEditable(False)
-            self.setCurrentText(new_text)
-            self.on_editing_finished.emit(new_text)
-
     def showPopup(self):
-        self.view().setMinimumWidth(self.sizeHint().width())
+        self.view().setMinimumWidth(self.view().sizeHintForColumn(0))
         self.before_popup_shown.emit()
         super().showPopup()
 
