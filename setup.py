d2a548aab895aa8ddfd977163ab7370c16347218
m3trik@live.com <m3trik@live.com>
2023-11-14 15:14:35 -0500

    1.0.2
    - update requirements.txt
    - switchboard: Added a method to remove saved states for a widget.

--- setup.py
+++ setup.py
@@ -12,6 +12,10 @@ description = ptk.get_text_between_delimiters(
     as_string=True,
 )
 
+# Read requirements.txt and add to install_requires
+with open("requirements.txt") as f:
+    required_packages = f.read().splitlines()
+
 setuptools.setup(
     name=__package__,
     version=__version__,
@@ -22,13 +26,14 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/m3trik/uitk",
-    packages=setuptools.find_packages(),  # scan the directory structure and include all package dependancies.
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
         "Operating System :: OS Independent",
     ],
+    packages=setuptools.find_packages(),  # scan the directory structure and include all package dependancies.
     include_package_data=True,
+    install_requires=required_packages,
     data_files=ptk.get_dir_contents(
         __package__, "filepath", exc_files=["*.py", "*.pyc", "*.json"]
     ),  # ie. ('uitk/ui/0', ['uitk/ui/0/init.ui']),
